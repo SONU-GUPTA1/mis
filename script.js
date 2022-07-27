b452d9170cca091d82879228765f9a240dc54b5f
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-07-27 19:10:11 +0530

    js quiz

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizDB=[
+    {
+        question: "Q1: What is the fullform of HTML?",
+        a: "HyperText Markup Langauge",
+        b: "Hypertext Markupe Langauge",
+        c: "Hypertext Markap Langauge",
+        d: "Hypertext Markup Langauge",
+        ans: "ans4"
+    },
+    {
+        question: "Q2: What is the fullform of CSS?",
+        a: "Cashcading style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Style seet",
+        d: "Cascade Style Sheets" ,
+        ans: "ans2"
+    },
+    {
+        question: "Q3: What is the fullform of JS?",
+        a: "JavaSystem",
+        b: "Javascript",
+        c: "JavaScript",
+        d:"JavaService" ,
+        ans: "ans3"
+    },
+    {
+        question: "Q4: What is the fullform of HTTP?",
+        a: "Hypertext Transfer Protocol",
+        b: "Hypertext Transport Protocol",
+        c: "HyperText Transfer Procedure",
+        d: "Hypertext Transfer Procedure",
+        ans: "ans1"
+    }
+];
